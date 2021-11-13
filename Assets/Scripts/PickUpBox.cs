757a6f9008e780b1372ee72d50a21fffa6d16ee1
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2021-11-13 01:59:48 +0100

    add box counter and script for counting

--- Assets/Scripts/PickUpBox.cs
+++ Assets/Scripts/PickUpBox.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PickUpBox : MonoBehaviour
 {
+    CounterController counterController;
     // Start is called before the first frame update
     void Start()
     {
-
+        counterController = GameObject.Find("Manager").GetComponent<CounterController>();
+        if(counterController == null)
+        {
+            Debug.LogError("CounterController not found.");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -15,6 +20,7 @@ public class PickUpBox : MonoBehaviour
         if (other.gameObject.name == "Jack")
         {
             Destroy(this.gameObject);
+            counterController.IncrementCounter();
         }
     }
 }
