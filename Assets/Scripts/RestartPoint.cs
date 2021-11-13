8ea4a45a8e94a95c619a95da4553103d1a3de8e1
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2021-11-13 05:02:15 +0100

    add start point prefab and scripts to save restart position

---
+++ Assets/Scripts/RestartPoint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartPoint : MonoBehaviour
+{
+    RestartPointsManager restartPointsManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restartPointsManager = GameObject.Find("Manager").GetComponent<RestartPointsManager>();
+        if (restartPointsManager == null)
+        {
+            Debug.LogError("RestartPointsManager not found.");
+        }
+    }
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            restartPointsManager.UpdateStartPoint(this.gameObject.transform);
+        }
+    }
+}
