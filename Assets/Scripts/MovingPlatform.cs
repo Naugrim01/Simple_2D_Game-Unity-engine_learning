0030f2b935c86fe37229741ac1c515782569ae36
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2021-11-16 21:15:19 +0100

    add MovingPlatform and modify script for moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     public float speed;
+
+    public Transform navStartPoint;
+    public Transform navEndPoint;
+
+    private Vector2 startPoint;
+    private Vector2 endPoint;
+
+    private Vector2 currentPlatformPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPoint = navStartPoint.position;
+        endPoint = navEndPoint.position;
+        Destroy(navStartPoint.gameObject);
+        Destroy(navEndPoint.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed, 0, 0);
+        currentPlatformPosition = Vector2.Lerp(startPoint, endPoint, Mathf.PingPong(Time.time * speed, 1));
+        //transform.Translate(speed, 0, 0);
+        transform.position = currentPlatformPosition;
     }
 
     void OnTriggerEnter2D(Collider2D other)
