32a1c874420b3a93ebfe40373c8817551d0ecacd
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2022-06-16 18:05:52 +0200

    add funtions for resolution settings

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle fullscreenTog, vsyncTog;
+
+    public ResItem[] resolutions;
+    
+    public int selsectedResolution;
+
+    public Text resolutionLabel;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +27,34 @@ public class OptionsMenu : MonoBehaviour
         
     }
 
+    public void ResLeft()
+    {
+        selsectedResolution--;
+        if (selsectedResolution < 0)
+        {
+            selsectedResolution = 0;
+        }
+        UpdateResLabel();
+    }
+    public void ResRight()
+    {
+        selsectedResolution++;
+        if (selsectedResolution > resolutions.Length - 1)
+        {
+            selsectedResolution = resolutions.Length - 1;
+        }
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selsectedResolution].horizontal.ToString() + " x " + resolutions[selsectedResolution].vertical.ToString();
+    }
+
     public void ApplyGraphics()
     {
         //apply fullscreen
-        Screen.fullScreen = fullscreenTog.isOn;
+        //Screen.fullScreen = fullscreenTog.isOn; - moved to "set the resolution"
        
         //apply vsync
 
@@ -34,5 +66,15 @@ public class OptionsMenu : MonoBehaviour
         {
             QualitySettings.vSyncCount = 0;
         }
+
+        //set the resolution
+
+        Screen.SetResolution(resolutions[selsectedResolution].horizontal, resolutions[selsectedResolution].vertical, fullscreenTog.isOn);
     }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
