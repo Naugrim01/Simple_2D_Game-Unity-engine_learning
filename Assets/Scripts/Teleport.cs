cc0ee012b6348526a56f23e5f66e2e1edfda3a10
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2022-06-24 19:18:21 +0200

    add Teleport prefub, loading level 2 scene and base for scene 2

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public string nextLevel;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.CompareTag("Player")) {
+
+            SceneManager.LoadScene(nextLevel);//
+            Time.timeScale = 1f;//
+
+        }
+
+    }
+
+
+}
